2207353aaa4449a55872b037212eb596b47cbee9
John Alob <john.alob@tecsurge.com>
2025-03-13 18:21:14 +0800

    continue training and practising OOP, modify rectangles programfile

--- Rectangles/Program.cs
+++ Rectangles/Program.cs
@@ -8,26 +8,76 @@
 
 //var calculator = new ShapesMeasurementCalculator();
 
-//var rectangle1 = new Rectangle(number1,number2);
+var rectangle1 = new Rectangle(2, 5);
 
-//Console.WriteLine("Width is " + rectangle1.Width);
-//Console.WriteLine("Height is " + rectangle1.Height);
-//Console.WriteLine("Circumference is :" + calculator.CalculateRectangleCircumference(rectangle1));
-//Console.WriteLine("Area is :" + calculator.CalcuateRectangleArea(rectangle1));
+Console.WriteLine("Width is " + rectangle1.Width);
+rectangle1.Width = 15;
+Console.WriteLine("Height is " + rectangle1.GetHeight());
+Console.WriteLine("Circumference is :" + rectangle1.CalculateRectangleCircumference());
+Console.WriteLine("Area is :" + rectangle1.CalcuateRectangleArea());
 
-//class Rectangle
+
+Console.ReadKey();
+
+class Rectangle
+{
+
+    public Rectangle(int width, int height)
+    {
+        Width = GetLengthorDefault(width, nameof(Width));
+        _height = GetLengthorDefault(height, nameof(_height));
+
+    }
+
+    public int Width { get; set; }
+ 
+
+
+    private int _height;
+
+    
+
+
+
+    public int GetHeight() => _height;
+
+    public void SetHeight(int value)
+    {
+        if(value > 0)
+        {
+            _height = value;
+        }
+    }
+
+
+    private int GetLengthorDefault(int length, string name)
+    {
+        const int DefaultValueIfNonPostive = 1;
+        if (length <= 0)
+        {
+            Console.WriteLine($"{name} must be a positive number.");
+            return DefaultValueIfNonPostive;
+        }
+        return length;
+    }
+
+    public int CalculateRectangleCircumference() => 2 * Width + 2 * _height;
+    public int CalcuateRectangleArea() => _height * Width;
+
+}
+
+
+
+//class ShapesMeasurementCalculator //Expression bodied method
 //{
-//    public int Width;
-//    public int Height;
+//    public int CalculateRectangleCircumference(Rectangle rectangle) => 2 * rectangle.Width + 2 * rectangle.Height;
 
-//    public Rectangle(int width, int height )
-//    {
-//        Width = width;
-//        Height = height;
-//    }
 
+//    public int CalcuateRectangleArea(Rectangle rectangle) => rectangle.Height * rectangle.Width;
 
 //}
+
+
 
 //class ShapesMeasurementCalculator
 //{
@@ -43,58 +93,76 @@
 //}
 
 
-var medicalAppointment = new MedicalAppointment(
-    "John Smith", new DateTime(2023, 4, 3));
-
-medicalAppointment.OverwriteMonthAndDay(5, 1);
-
-medicalAppointment.MoveByMonthsAndDays(7, 1);
 
 
 
 
 
-Console.ReadKey();
+////var medicalAppointment = new MedicalAppointment(
+////    "John Smith", new DateTime(2023, 4, 3));
 
-class MedicalAppointment
-{
-    public string _patientName;
-    public DateTime _date;
+////medicalAppointment.Reschedule(new DateTime(2023, 4, 5));
 
-    public MedicalAppointment(string patientName, DateTime date)
-    {
-        _patientName = patientName;
-        _date = date;
-    }
+////var appointmentTwoweeksFromNow = new MedicalAppointment("Bob Smith", 14);
 
-    public MedicalAppointment(string patientName) :
-        this(patientName, 7)
-    {
-       
-    }
+////var appointmentOneweeksFromNow = new MedicalAppointment("Marga Smith");
 
-    public MedicalAppointment(string patientName, int daysFromNow)
-    {
-        _patientName = patientName;
-        _date = DateTime.Now.AddDays(daysFromNow);
-    }
+//var onlyname = new MedicalAppointment("name only");
 
 
-    public void Reschedule(DateTime date)
-    {
-        _date = date;
-    }
+//class MedicalAppointmentPrinter
+//{
+//    public void Print(MedicalAppointment medicaAppointment)
+//    {
+//        Console.WriteLine("Appointment will take place on " + medicaAppointment.GetDate());
+//    }
+//}
 
-    public void OverwriteMonthAndDay(int month, int day)
-    {
-        _date = new DateTime(_date.Year, month, day);
-    }
 
-    public void MoveByMonthsAndDays(int monthsToAdd, int daysToAdd)
-    {
-        _date = new DateTime(
-            _date.Year,
-            _date.Month + monthsToAdd,
-            _date.Day + daysToAdd);
-    }
-}
+//class MedicalAppointment
+//{
+//    public string _patientName;
+//    public DateTime _date;
+
+//    public MedicalAppointment(string patientName, DateTime date)
+//    {
+//        _patientName = patientName;
+//        _date = date;
+//    }
+
+//    public DateTime GetDate() => _date;
+
+//    //public MedicalAppointment(string patientName) :
+//    //    this(patientName, 7)
+//    //{
+
+//    //}
+
+
+//    public MedicalAppointment(string patientName = "Unknown", int daysFromNow = 7)
+//    {
+//        _patientName = patientName;
+//        _date = DateTime.Now.AddDays(daysFromNow);
+//    }
+
+
+//    public void Reschedule(DateTime date)
+//    {
+//        _date = date;
+//        var printer = new MedicalAppointmentPrinter();
+//        printer.Print(this);
+//    }
+
+//    public void OverwriteMonthAndDay(int month, int day)
+//    {
+//        _date = new DateTime(_date.Year, month, day);
+//    }
+
+//    public void MoveByMonthsAndDays(int monthsToAdd, int daysToAdd)
+//    {
+//        _date = new DateTime(
+//            _date.Year,
+//            _date.Month + monthsToAdd,
+//            _date.Day + daysToAdd);
+//    }
+//}
