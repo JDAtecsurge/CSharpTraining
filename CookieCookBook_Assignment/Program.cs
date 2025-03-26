a0e2e5a5a97c235734b6dcc0f4fe9c00cac0bdd3
John Alob <john.alob@tecsurge.com>
2025-03-26 18:25:21 +0800

    continued training

--- CookieCookBook_Assignment/Program.cs
+++ CookieCookBook_Assignment/Program.cs
@@ -6,26 +6,33 @@ using CookieCookBook_Assignment.Recipes.Ingredients;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
+try
+{
+    const FileFormat Format = FileFormat.Json;
 
-const FileFormat Format = FileFormat.Json;
+    IStringsRepository stringsRepository = Format == FileFormat.Json ?
+        new StringsJsonRepository() :
+        new StringsTextualRepository();
 
-IStringsRepository stringsRepository = Format == FileFormat.Json ?
-    new StringsJsonRepository() :
-    new StringsTextualRepository();
-
-const string FileName = "recipes";
-var fileMetaData = new FileMetaData(FileName, Format);
+    const string FileName = "recipes";
+    var fileMetaData = new FileMetaData(FileName, Format);
 
 
-var ingredientsRegister = new IngredientsRegister();
+    var ingredientsRegister = new IngredientsRegister();
 
-var cookiesRecipesApp = new CookiesRecipesApp(
-        new RecipesRepository(
-            new StringsJsonRepository(), 
-            ingredientsRegister), 
-        new RecipesConsoleUserInteraction(
-            ingredientsRegister));
+    var cookiesRecipesApp = new CookiesRecipesApp(
+            new RecipesRepository(
+                new StringsJsonRepository(),
+                ingredientsRegister),
+            new RecipesConsoleUserInteraction(
+                ingredientsRegister));
 
-cookiesRecipesApp.Run(fileMetaData.ToPath());
-//cookiesRecipesApp.Run("recipes.json");
+    cookiesRecipesApp.Run(fileMetaData.ToPath());
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred: " + ex.Message);
 
+    Console.WriteLine("Press any key to close.");
+    Console.ReadKey();  
+}
--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -3,18 +3,27 @@
 //try
 //{
 //    int number = ParseStringToInt(input);
-//    var result = 10 / number; 
+//    var result = 10 / number;
 //    Console.WriteLine($"10/{number} is" + result);
 
 //}
-//catch( FormatException ex )
+//catch (FormatException ex)
 //{
-//    Console.WriteLine("Wrong format. Input string is not parsable to int. " + 
+//    Console.WriteLine("Wrong format. Input string is not parsable to int. " +
 //        "Exception message: " + ex.Message);
 
 //}
-//catch(DivideByZeroException ex)
+//catch (DivideByZeroException ex)
 //{
+//    try
+//    {
+//       int.Parse("not an int");
+//    }
+//    catch (FormatException ex2)
+//    {
+
+//    }
+
 //    Console.WriteLine("Division by zero is not allowed. " +
 //        "Exception message: " + ex.Message);
 //}
@@ -51,73 +60,174 @@
 
 //RecursiveMethod(11);
 
-try
-{
-    var result = IsFirstElementPresent(null);    
-}
-catch (ArgumentNullException ex)
-{
+//try
+//{
+//    var result = IsFirstElementPresent(null);    
+//}
+//catch (ArgumentNullException ex)
+//{
 
-}
+//}
 
+
+//try
+//{
+//    var dataFromweb = SendHttpRequest("www.someAddress.com/get/someResource");
+//}
+//catch(HttpRequestException ex) when (ex.Message == "403")
+//{
+//    Console.WriteLine("It was forbidden to access the resource.");
+//    throw;
+//}
+//catch(HttpRequestException ex) when (ex.Message == "404")
+//{
+//    Console.WriteLine("The resource was not found.");
+//    throw;
+//}
+//catch(HttpRequestException ex) when (ex.Message.StartsWith("4"))
+//{
+//    Console.WriteLine("Some kind of client error.");
+//    throw;
+//}
+//catch(HttpRequestException ex) when (ex.Message == "500")
+//{
+//    Console.WriteLine("The server has experienced an internal error.");
+//    throw;
+//}
+
+//using System.Runtime.Serialization;
+
+//throw new Exception();
+
+//try
+//{
+//    ComplexMethod();
+//}
+//catch(InvalidOperationException ex) when (ex.Message == "Cannot connect to a service.")
+//{
+//    Console.WriteLine("Check your Internet connection");
+//    throw;
+//}
 
 
 
 Console.ReadKey();
+
+
+
+//[Serializable]
+//public class CustomException : Exception
+//{
+
+//    public int StatusCode { get; }
+
+//    protected CustomException(
+//        SerializationInfo info,
+//        StreamingContext context) : base(info, context)
+//    {
+
+//    }
+
+//    public CustomException()
+//    {
+
+//    }
+//    public CustomException(string message, int statusCode) : base(message)
+//    {
+//        StatusCode = statusCode;
+//    }
+//    public CustomException(
+//        string message, 
+//        int statusCode, 
+//        Exception innerException) : base(message, innerException)
+//    {
+//        StatusCode = statusCode;
+//    }
+
+//    public CustomException(string message) : base(message)
+//    {
+
+//    }
+//    public CustomException(string message, Exception innerException) 
+//        : base(message, innerException) 
+//    {
+
+//    }
+
+//}
+
+
 
 //int ParseStringToInt(string input)
 //{
 //    return int.Parse(input);
 //}
 
-void RecursiveMethod(int counter)
-{
-    Console.WriteLine("Recursive method is being executed" + "Counter is: " + counter);
-    if (counter < 10)
-    {
-        RecursiveMethod(counter+1);
-    }
-}
+//void RecursiveMethod(int counter)
+//{
+//    Console.WriteLine("Recursive method is being executed" + "Counter is: " + counter);
+//    if (counter < 10)
+//    {
+//        RecursiveMethod(counter+1);
+//    }
+//}
 
-int GetFirstElement(IEnumerable<int> numbers)
-{
-    foreach (var number in numbers)
-    {
-        return number;
-    }
+//int GetFirstElement(IEnumerable<int> numbers)
+//{
+//    foreach (var number in numbers)
+//    {
+//        return number;
+//    }
 
-    throw new InvalidOperationException("the collection cannot be empty.");
-}
+//    throw new InvalidOperationException("the collection cannot be empty.");
+//}
 
-bool IsFirstElementPresent(IEnumerable<int> numbers)
-{
-    try
-    {
-        var firstElement = GetFirstElement(numbers);
-        return firstElement > 0;
-    }
-    catch (InvalidOperationException ex)
-    {
-        Console.WriteLine("the collection is empty ");
-        return true;
-    }
-    catch(NullReferenceException ex)
-    {
-        Console.WriteLine("Apllication experienced an error");
-        //throw;
-        throw new ArgumentNullException("The collection cannot be null.", ex);
-    }
-    
-}
+//bool IsFirstElementPresent(IEnumerable<int> numbers)
+//{
+//    try
+//    {
+//        var firstElement = GetFirstElement(numbers);
+//        return firstElement > 0;
+//    }
+//    catch (InvalidOperationException ex)
+//    {
+//        Console.WriteLine("the collection is empty ");
+//        return true;
+//    }
+//    catch(NullReferenceException ex)
+//    {
+//        Console.WriteLine("Apllication experienced an error");
+//        //throw;
+//        throw new ArgumentNullException("The collection cannot be null.", ex);
+//    }
+
+//}
+
+//void ComplexMethod()
+//{
+//    //step 1: connecting
+//    throw new ConnectionException("Cannot connect to a service.");
+
+//    //step 2: authorizing
+//    throw new AuthorizationException("Cannot authoruze the user");
+
+//    //step 3: retrieving data as JSON
+//    throw new DataAccessException("Cannot retrieve data.");
+
+//    //step 4: parsing the JSON to some C# type
+//    throw new JsonParingException("Cannot parse JSON data.");
+
+//}
+
 
 class Person
 {
-    public string Name { get;}
-    public int YearofBirth { get;}
+    public string Name { get; }
+    public int YearofBirth { get; }
 
     public Person(string name, int yearofBirth)
     {
-        if(name == null)
+        if (name == null)
         {
             throw new ArgumentNullException("The name cannot be null.");
         }
@@ -126,9 +236,9 @@ class Person
         {
             throw new ArgumentException("The name cannot be empty.");
         }
-        if(yearofBirth < 1900 || yearofBirth > DateTime.Now.Year)
+        if (yearofBirth < 1900 || yearofBirth > DateTime.Now.Year)
         {
-            throw new ArgumentOutOfRangeException("The year of birth must be " + 
+            throw new ArgumentOutOfRangeException("The year of birth must be " +
                 "between 1900 and the current year.");
         }
         Name = name;
@@ -136,3 +246,4 @@ class Person
     }
 
 }
+
